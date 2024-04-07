3fbf553c7f55a4e85d1b4586f1f39cb170ea039e
Atul Tingre <atultingre.work@gmail.com>
2024-04-07 12:50:46 +0530

    initial setup

---
+++ index.js
@@ -0,0 +1,36 @@
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import dotenv from "dotenv";
+import express from "express";
+import morgan from "morgan";
+import dbConnection from "./utils/index.js";
+import { errorHandler, routeNotFound } from "./middlewares/error.middleware.js";
+const routes = "";
+
+dotenv.config();
+
+dbConnection();
+
+const PORT = process.env.PORT || 8000;
+const app = express();
+
+app.use(
+  cors({
+    origin: ["http://localhost:5173"],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(morgan("dev"));
+
+// app.use("/api", routes);
+app.use(routeNotFound);
+app.use(errorHandler);
+
+app.listen(PORT, () =>
+  console.log(`Server listening on port: http://localhost:${PORT}`)
+);
