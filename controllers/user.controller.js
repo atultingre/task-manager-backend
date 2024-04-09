18720f4260e2b7d89523c4d898a5abce99f60b97
Atul Tingre <atultingre.work@gmail.com>
2024-04-09 18:18:25 +0530

    update user profile

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -120,3 +120,40 @@ export const getNotificationsList = async (req, res) => {
     return res.status(400).json({ status: false, message: error.message });
   }
 };
+
+export const updateUserProfile = async (req, res) => {
+  try {
+    const { userId, isAdmin } = req.user;
+    const { _id } = req.body;
+
+    const id =
+      isAdmin && userId === _id
+        ? userId
+        : isAdmin && userId !== _id
+        ? _id
+        : userId;
+
+    const user = await User.findById(id);
+
+    if (user) {
+      user.name = req.body.name || user.name;
+      user.title = req.body.title || user.title;
+      user.role = req.body.role || user.role;
+
+      const updatedUser = await user.save();
+
+      user.password = undefined;
+
+      res.status(201).json({
+        status: true,
+        message: "Profile Updated Successfully.",
+        user: updatedUser,
+      });
+    } else {
+      res.status(404).json({ status: false, message: "User not found" });
+    }
+  } catch (error) {
+    console.log(error);
+    return res.status(400).json({ status: false, message: error.message });
+  }
+};
