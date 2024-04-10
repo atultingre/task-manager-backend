91e526b9d24173924f5828dfc27d3c78614c0da4
Atul Tingre <atultingre.work@gmail.com>
2024-04-10 23:49:19 +0530

    post activity

--- routes/task.routes.js
+++ routes/task.routes.js
@@ -1,12 +1,12 @@
 import express from "express";
-import { createTask, duplicateTask } from "../controllers/task.controller.js";
+import { createTask, duplicateTask, postTaskActivity } from "../controllers/task.controller.js";
 import { isAdminRoute, protectRoute } from "../middlewares/auth.middleware.js";
 
 const router = express.Router();
 
 router.post("/create", protectRoute, isAdminRoute, createTask);
 router.post("/duplicate/:id", protectRoute, isAdminRoute, duplicateTask);
-// router.post("/activity/:id", protectRoute, postTaskActivity);
+router.post("/activity/:id", protectRoute, postTaskActivity);
 
 // router.get("/dashboard", protectRoute, dashboardStatistics);
 // router.get("/", protectRoute, getTasks);
