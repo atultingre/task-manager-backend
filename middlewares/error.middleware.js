c9aeff41d53ae41897a0d88e1d55df028e8fb928
Atul Tingre <atultingre.work@gmail.com>
2024-04-12 01:14:20 +0530

    bug fix : login register

--- middlewares/error.middleware.js
+++ middlewares/error.middleware.js
@@ -8,14 +8,14 @@ const errorHandler = (err, req, res, next) => {
   let statusCode = res.statusCode === 200 ? 500 : res.statusCode;
   let message = err.message;
 
-  if (err.message === "CastError") {
+  if (err.name === "CastError" && err.kind === "ObjectId") {
     statusCode = 404;
     message = "Resource not found";
   }
 
-  req.status(statusCode).json({
+  res.status(statusCode).json({
     message: message,
-    stack: process.env.NODE_ENV !== "production" ? null : err.stack,
+    stack: process.env.NODE_ENV === "production" ? null : err.stack,
   });
 };
 
--- models/user.model.js
+++ models/user.model.js
@@ -19,11 +19,12 @@ userSchema.pre("save", async function (next) {
   if (!this.isModified("password")) {
     next();
   }
-  const salt = await bcrypt.genSalt(process.env.SALT);
+
+  const salt = await bcrypt.genSalt(10);
   this.password = await bcrypt.hash(this.password, salt);
 });
 
-userSchema.method.matchPassword = async function (enteredPassword) {
+userSchema.methods.matchPassword = async function (enteredPassword) {
   return await bcrypt.compare(enteredPassword, this.password);
 };
 
