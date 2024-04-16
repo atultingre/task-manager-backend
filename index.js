5c7c6b0074a8fb81cd182feab3bff49148d6eec2
Atul Tingre <atultingre.work@gmail.com>
2024-04-16 11:50:26 +0530

    site origin added

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: ["http://localhost:5173", "https://at-task-manager.netlify.app"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
